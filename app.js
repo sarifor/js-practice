818af229a8f33e4f075c3c65ab5964e75e1ce5fa
sarifor <iread1q84@gmail.com>
2023-04-08 22:12:01 +0900

    Use Youtube Data API v3 to fetch comments of a video

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+const axios = require("axios")
 const express = require('express')
+const { YOUTUBE_API_KEY } =require('./apikeys.js')
 const app = express()
 const port = 3000
 
+console.log(YOUTUBE_API_KEY);
+
+const getData = async () => {
+    const baseURL = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key=" + YOUTUBE_API_KEY + "&videoId=Z9eqBrp_uR0&maxResults=3";
+
+    const client = axios.create({
+        baseURL: baseURL
+    });
+
+    const response = await client.get();
+    console.log(response);
+}
+
 app.get('/', (req, res) => {
+    getData();
     res.send('Hello World!')
 })
 
