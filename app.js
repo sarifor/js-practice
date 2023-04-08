89e67d30f819c0c1f45e1e537b3d8e49bed75282
sarifor <iread1q84@gmail.com>
2023-04-08 20:55:34 +0900

    Indent codes more

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log('Go to http://localhost:3000/')
+    console.log('Go to http://localhost:3000/')
 })
