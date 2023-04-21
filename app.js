eb29a5ded29fe623adc52f143d809ff2fa6d08b3
sarifor <iread1q84@gmail.com>
2023-04-21 09:39:22 +0900

    Test if request is successful

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const axios = require("axios");
+// require("dotenv").config();
+// const axios = require("axios");
 const express = require('express');
 const cors = require("cors");
 const app = express();
 const port = 3000;
 
-const getData = async () => {
+/* const getData = async () => {
     try {
         const baseURL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.API_KEY}`
 
@@ -18,17 +18,21 @@ const getData = async () => {
     } catch (e) {
         console.log(e);
     }
-}
+} */
 
 app.use(cors());
 
-app.get('/', async (req, res) => {
+/* app.get('/', async (req, res) => {
     try {
         const response = await getData();
         res.send(response.data);
     } catch (e) {
         console.log(e);
     }
+}) */
+
+app.get('/', (req, res) => {
+    res.send("test");
 })
 
 app.listen(port, () => {
