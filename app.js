08babc95366e16294680aa937a8d940c45b108b0
sarifor <iread1q84@gmail.com>
2023-04-21 12:58:29 +0900

    Test if environment variables can be used on GCP

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const app = express();
 const port = 3000;
 const secretmanagerClient = new SecretManagerServiceClient();
 
-/* const getData = async () => {
+let API_KEY;
+
+const getData = async () => {
     try {
-        const baseURL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.API_KEY}`
+        const baseURL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${API_KEY}`
 
         const client = axios.create({
             baseURL: baseURL
@@ -20,35 +22,31 @@ const secretmanagerClient = new SecretManagerServiceClient();
     } catch (e) {
         console.log(e);
     }
-} */
+}
 
 app.use(cors());
 
-/* app.get('/', async (req, res) => {
+async function callAccessSecretVersion() {
+    try {
+        const request = {
+            name: "projects/694909544055/secrets/API_KEY/versions/1",
+        };
+      
+        const response = await secretmanagerClient.accessSecretVersion(request);
+        API_KEY = response[0].payload.data.toString('utf-8'); // index starts from 0
+    } catch (e) {
+        console.log(e);
+    }
+}
+callAccessSecretVersion();   
+
+app.get('/', async (req, res) => {
     try {
         const response = await getData();
         res.send(response.data);
     } catch (e) {
         console.log(e);
     }
-}) */
-
-app.get('/', (req, res) => {
-    async function callAccessSecretVersion() {
-        try {
-            const request = {
-                name: "projects/694909544055/secrets/API_KEY/versions/1",
-            };
-          
-            const response = await secretmanagerClient.accessSecretVersion(request);
-            console.log(response[0].payload.data.toString('utf-8')); // index starts from 0
-        } catch (e) {
-            console.log(e);
-        }
-    }
-    callAccessSecretVersion();   
-
-    res.send("test");
 })
 
 app.listen(port, () => {
