07503fdbd283fb84e79b6e69d26f13b133891273
sarifor <iread1q84@gmail.com>
2023-04-21 11:48:08 +0900

    Test if environment variables can be used on GCP

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // const axios = require("axios");
 const express = require('express');
 const cors = require("cors");
+const googleCloud = require("import secretmanager");
 const app = express();
 const port = 3000;
 
@@ -32,7 +33,10 @@ app.use(cors());
 }) */
 
 app.get('/', (req, res) => {
-    console.log(process.env);
+    secret_resource_name = "projects/694909544055/secrets/API_KEY/versions/1"
+    response = gcp_secret_client.access_secret_version(secret_resource_name)
+    secret_value = response.payload.data.decode('UTF-8')
+    console.log(secret_value);
     res.send("test");
 })
 
