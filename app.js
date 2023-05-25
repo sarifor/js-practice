5f1fd05de5205ea0b1b9b57a8b9151256f505309
sarifor <iread1q84@gmail.com>
2023-05-25 15:09:04 +0900

    Try accessing vm instance through loadbalancer

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager').v1;
 const app = express();
-const port = 3000;
+const port = 80;
 const secretmanagerClient = new SecretManagerServiceClient();
 
 let API_KEY;
@@ -47,5 +47,5 @@ app.get('/', async (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Go to http://35.202.176.94:3000/')
+    console.log('Go to http://35.202.176.94:80/')
 })
